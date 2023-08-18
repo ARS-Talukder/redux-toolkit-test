3f2eaa810e1a202cccd7fa2b75824dda8b0ae4e3
ARS Talukder <talukderars@gmail.com>
2023-08-18 10:13:03 +0600

    payload removed

--- src/features/counter/counterSlice.jsx
+++ src/features/counter/counterSlice.jsx
@@ -13,7 +13,7 @@ export const counterSlice = createSlice({
 
         },
         decrement: (state, action) => {
-            state.count = state.count - action.payload;
+            state.count = state.count - 1;
         }
     },
 })
